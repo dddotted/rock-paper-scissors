2b6aa2153b5417c85b42bc846a043fbeb2baee5d
dddotted <49547278+dddotted@users.noreply.github.com>
2024-12-20 17:43:58 +0900

    feat: 処理

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const choice = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+  return choice[Math.floor(Math.random() * choice.length)];
+}
+
+function getHumanChoice(shout = "rock, paper, scissors, shoot") {
+  const input = prompt(shout);
+  const humanChoice = (typeof input === 'string') ? input.toLowerCase() : input;
+  if (choice.indexOf(humanChoice) === -1) {
+    getHumanChoice();
+  } else {
+    return humanChoice;
+  }
+}
+
+function playRound(humanChoice, computerChoice) {
+  console.log(humanChoice);
+  console.log(computerChoice);
+  if (humanChoice === computerChoice) {
+    playRound(getHumanChoice("It’s a tie"), getComputerChoice());
+  } else {
+    const humanWinFlag = ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'scissors' && computerChoice === 'paper') || (humanChoice === 'paper' && computerChoice === 'rock'));
+
+    console.log(`You ${(humanWinFlag) ? 'Win' : 'Lose'}! ${humanWinFlag ? humanChoice : computerChoice} beats ${humanWinFlag ? computerChoice : humanChoice}.`);
+    return (humanWinFlag);
+  }
+}
+
+
+
+//  playRoundを5回繰り返す
+// コンソールに勝者と得点を表示する
+function playGame() {
+  const count = 5;
+  let score;
+  // for (let i = 0; i < count; i++) {
+  //   score += playRound(getHumanChoice(), getComputerChoice());
+  //   console.log(score);
+  // }
+}
+
+playGame();
